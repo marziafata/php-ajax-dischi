ca4e9bd2fea43bbb4000f65c29ba8be3ae5b0854
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-17 15:29:15 +0200

    create cartelle laravel mix per versione-php

---
+++ versione-php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Dischi PHP</title>
+        <link rel="stylesheet" href="public/app.css">
+    </head>
+    <body>
+
+
+        <!-- javascript e jquery -->
+        <script src="public/app.js" charset="utf-8"></script></body>
+    </body>
+</html>
