6d79bfa16926a213486bd50fdd00815d900261db
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-18 12:33:56 +0200

    commenti

--- public/app.js
+++ public/app.js
@@ -10982,21 +10982,27 @@ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"
 $(document).ready(function () {
   //dichiaro le variabili che mi serviranno con Handlebars
   var source = $('#entry-template').html();
-  var template = Handlebars.compile(source);
+  var template = Handlebars.compile(source); //faccio partire la chiamata ajax
+
   $.ajax({
     'url': 'dischi.php',
+    //percorso file che contiene l'array di dati che mi serve (se non funziona potrei diver scrivere il percorso a partire da localhost)
     'method': 'GET',
     'success': function success(data) {
+      //ciclo l'array per tirare fuori i singoli elementi
       for (var index = 0; index < data.length; index++) {
-        var element = data[index];
-        console.log(element);
+        //salvo l'elemento corrente in una variabile
+        var element = data[index]; //creo l'oggetto context che contiene le proprietà (compilate con la dot notation) da dare ad Handlebars per compilare le card
+
         var context = {
           "copertina": element.poster,
           "titolo": element.title,
           "autore": element.author,
           "anno": element.year
-        };
-        var disco = template(context);
+        }; // Creo la variabile delle card che contengono il template compilato con handlebars
+
+        var disco = template(context); //appendo le card al container già creato
+
         $('main.container').append(disco);
       }
     },
--- src/app.js
+++ src/app.js
@@ -6,14 +6,17 @@ $(document).ready(function() {
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
     
+    //faccio partire la chiamata ajax
     $.ajax({
-        'url': 'dischi.php',
+        'url': 'dischi.php',//percorso file che contiene l'array di dati che mi serve (se non funziona potrei diver scrivere il percorso a partire da localhost)
         'method': 'GET',
         'success': function(data) {
+            //ciclo l'array per tirare fuori i singoli elementi
             for (let index = 0; index < data.length; index++) {
+                //salvo l'elemento corrente in una variabile
                 const element = data[index];
-                console.log(element);
-
+                
+                //creo l'oggetto context che contiene le proprietà (compilate con la dot notation) da dare ad Handlebars per compilare le card
                 var context = {
                     "copertina" : element.poster,
                     "titolo" : element.title,
@@ -21,12 +24,11 @@ $(document).ready(function() {
                     "anno" : element.year
                 };
 
+                // Creo la variabile delle card che contengono il template compilato con handlebars
                 var disco = template(context);
 
+                //appendo le card al container già creato
                 $('main.container').append(disco);
-                
-            
-                
             }   
         },
         'error': function() {
